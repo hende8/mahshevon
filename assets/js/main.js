bffbc2f62bae1000474894f4b00b2f6d01cc88b1
hende8 <hendebii@gmail.com>
2024-08-09 18:10:40 +0300

    zoom input

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,15 @@
-/*===== MENU SHOW =====*/ 
-const showMenu = (toggleId, navId) =>{
+/*===== MENU SHOW =====*/
+const showMenu = (toggleId, navId) => {
     const toggle = document.getElementById(toggleId),
-    nav = document.getElementById(navId)
+        nav = document.getElementById(navId)
 
-    if(toggle && nav){
-        toggle.addEventListener('click', ()=>{
+    if (toggle && nav) {
+        toggle.addEventListener('click', () => {
             nav.classList.toggle('show')
         })
     }
 }
-showMenu('nav-toggle','nav-menu')
+showMenu('nav-toggle', 'nav-menu')
 
 function calculatePercentage() {
     const number = parseFloat(document.getElementById('number').value);
@@ -28,17 +28,17 @@ function calculatePercentage() {
 
 function calculateVAT() {
     var number = document.getElementById('inputMaamNumber').value;
-    
+
     var vatAmount = number * 0.17;
-    
+
     var total = parseFloat(number) + vatAmount;
-    
-    document.getElementById('resultMaam').innerText = 'הסכום עם מע"מ: ' + total.toFixed(2) + ' ש"ח';
+
+    document.getElementById('resultMaam').innerText = 'הסכום כולל מע"מ: ' + total.toFixed(2) + ' ש"ח';
 }
 
 function calculateInchToCm() {
     var inches = document.getElementById('inputInches').value;
-    
+
     var centimeters = inches * 2.54;
     document.getElementById('resultInchToCm').innerHTML = '<span dir="rtl">' + inches + ' אינץ\' הם ' + centimeters.toFixed(2) + ' ס"מ</span>';
 
@@ -47,7 +47,7 @@ function calculateInchToCm() {
 /*==================== REMOVE MENU MOBILE ====================*/
 const navLink = document.querySelectorAll('.nav__link')
 
-function linkAction(){
+function linkAction() {
     const navMenu = document.getElementById('nav-menu')
     // When we click on each nav__link, we remove the show-menu class
     navMenu.classList.remove('show')
@@ -57,20 +57,20 @@ navLink.forEach(n => n.addEventListener('click', linkAction))
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 const sections = document.querySelectorAll('section[id]')
 
-const scrollActive = () =>{
+const scrollActive = () => {
     const scrollDown = window.scrollY
 
-  sections.forEach(current =>{
+    sections.forEach(current => {
         const sectionHeight = current.offsetHeight,
-              sectionTop = current.offsetTop - 58,
-              sectionId = current.getAttribute('id'),
-              sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
-        
-        if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
+            sectionTop = current.offsetTop - 58,
+            sectionId = current.getAttribute('id'),
+            sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+
+        if (scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
             sectionsClass.classList.add('active-link')
-        }else{
+        } else {
             sectionsClass.classList.remove('active-link')
-        }                                                    
+        }
     })
 }
 window.addEventListener('scroll', scrollActive)
@@ -84,15 +84,15 @@ const sr = ScrollReveal({
     reset: true
 });
 
-sr.reveal('.home__data, .about__img, .skills__subtitle, .skills__text',{}); 
-sr.reveal('.home__img, .about__subtitle, .about__text, .skills__img',{delay: 400}); 
-sr.reveal('.home__social-icon',{ interval: 200}); 
-sr.reveal('.skills__data, .work__img, .contact__input',{interval: 200}); 
-document.addEventListener('DOMContentLoaded', function() {
+sr.reveal('.home__data, .about__img, .skills__subtitle, .skills__text', {});
+sr.reveal('.home__img, .about__subtitle, .about__text, .skills__img', { delay: 400 });
+sr.reveal('.home__social-icon', { interval: 200 });
+sr.reveal('.skills__data, .work__img, .contact__input', { interval: 200 });
+document.addEventListener('DOMContentLoaded', function () {
     const navToggle = document.getElementById('nav-toggle');
     const navMenu = document.getElementById('nav-menu');
-  
-    navToggle.addEventListener('click', function() {
-      navMenu.classList.toggle('show');
+
+    navToggle.addEventListener('click', function () {
+        navMenu.classList.toggle('show');
     });
-  });
+});
